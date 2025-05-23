906df5b9117a4b24457c715ad3e519c267f6b313
Veliae <hvwnight@gmail.com>
2025-05-23 10:54:59 -0400

    Update repotest.java

--- repotest.java
+++ repotest.java
@@ -4,5 +4,7 @@ public class repotest{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("hello world");
+		con.println("TGFI");
+		con.println("YAY cpt");
 	}
 }
