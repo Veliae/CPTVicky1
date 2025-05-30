6be66acaf87b6cf966b5422a32edaf7b5f3dc71e
Veliae <hvwnight@gmail.com>
2025-05-30 10:51:37 -0400

    Update VideoPoker.java

--- VideoPoker.java
+++ VideoPoker.java
@@ -1,9 +1,22 @@
 import arc.*;
+import java.awt.Color;
 
 public class VideoPoker{
 	public static void main (String[] args) {
-		Console con = new Console();
+		Console con = new Console("Poker", 1400, 800);
+		int intX = 300;
+		int intY = 200;
 		
+		while(intX > 0){ 
+			//Draws Baclground before drawing all the elements at the top
+			con.setDrawColor(Color.BLACK);
+			con.setDrawColor(new Color(82, 20, 31));
+			con.fillRect(30, 30, 1400, 800);
+			
+			con.setDrawColor(new Color(101, 50, 86));
+			con.fillRoundRect(400 , 400, 100, 100, 400, 400);
+			
+		}
 		}
 }
 
