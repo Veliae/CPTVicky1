c3bddade977dd71b611516b01044e8717ad7c82e
Veliae <hvwnight@gmail.com>
2025-06-02 11:02:28 -0400

    menu added
    added the homepage

--- VideoPoker.java
+++ VideoPoker.java
@@ -1,22 +1,20 @@
 import arc.*;
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 public class VideoPoker{
 	public static void main (String[] args) {
 		Console con = new Console("Poker", 1400, 800);
-		int intX = 300;
-		int intY = 200;
-		
-		while(intX > 0){ 
-			//Draws Baclground before drawing all the elements at the top
-			con.setDrawColor(Color.BLACK);
-			con.setDrawColor(new Color(82, 20, 31));
-			con.fillRect(30, 30, 1400, 800);
-			
-			con.setDrawColor(new Color(101, 50, 86));
-			con.fillRoundRect(400 , 400, 100, 100, 400, 400);
-			
-		}
-		}
+		BufferedImage imgpokermenu = con.loadImage("pokermenu.png");
+		 
+
+        // Calculate centered position
+        int intX = (1400 - imgpokermenu.getWidth()) / 2;
+        int intY = (800 - imgpokermenu.getHeight()) / 2;
+
+        // Draw only the centered image
+        con.drawImage(imgpokermenu, intX, intY);
+        con.repaint();
+    }
 }
 
