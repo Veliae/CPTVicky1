8a89b7a5bacdb6d2dea8fd824d193c8b41d87408
Veliae <hvwnight@gmail.com>
2025-06-09 20:00:16 -0400

    testing
    - figure out bubble sort in the new java file first

---
+++ TestingPoker.java
@@ -0,0 +1,7 @@
+import arc.*;
+public class TestingPoker {
+    public static void main(String[] args) {
+        Console con = new Console();
+        
+	}
+}
--- VideoPoker.java
+++ VideoPoker.java
@@ -31,5 +31,6 @@ public class VideoPoker {
                 bolWaiting = false;
             }
         }
+        
     }
 }
