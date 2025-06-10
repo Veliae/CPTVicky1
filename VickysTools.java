08fdc021fea0e7a6bb8c33ef924a3f13b9e0c2f1
Veliae <hvwnight@gmail.com>
2025-06-10 09:36:35 -0400

    Sorting cards (loop done)
    - have some issues committing but sorting cards works (worked earlier now doesn't, will find out why asap)
    - still need to somehow add 3 popup screens + create new screen for leaderboard  (will design in canva if time allows)

--- VickysTools.java
+++ VickysTools.java
@@ -98,4 +98,51 @@ public class VickysTools {
 			con.sleep(3000);
 			con.closeConsole();
 		}
+			// Card deck arrays - 52 cards x 3 columns (value, suit, random number for sorting)
+		public static int[][] intDeck = new int[52][3];
+		// 5 cards x 2 (value, suit)
+		public static int[][] intHand = new int[5][2]; 
+		// Track which cards are used
+		public static boolean[] bolUsedCards = new boolean[52]; 
+		
+		public static void drawCenteredString(Console con, String strText, int intY) {
+			int intCharWidth = 20; 
+			int intTextWidth = strText.length() * intCharWidth;
+			int intX = (1392 - intTextWidth) / 2;
+			con.drawString(strText, intX, intY);
+		}
+			
+			// Initialize and shuffle the deck
+		public static void prepareDeck() {
+			// Fill deck with cards
+			int intCardIndex = 0;
+			// 1=diamonds, 2=clubs, 3=hearts, 4=spades
+			for (int intSuit = 1; intSuit <= 4; intSuit++) { 
+				// 1-13 card values
+				for (int intValue = 1; intValue <= 13; intValue++) {
+					// Column 1: value 
+					intDeck[intCardIndex][0] = intValue; 
+					// Column 2: suit
+					intDeck[intCardIndex][1] = intSuit; 
+					// Column 3: random number for sorting 
+					intDeck[intCardIndex][2] = (int)(Math.random() * 100) + 1; 
+					intCardIndex++;
+			}
+		}
+		// Bubble sort by random number (column 3) to shuffle
+		for (int i = 0; i < 51; i++) {
+			for (int j = 0; j < 51 - i; j++) {
+				if (intDeck[j][2] > intDeck[j + 1][2]) {
+					// Swap entire rows
+					int[] intTemp = intDeck[j];
+					intDeck[j] = intDeck[j + 1];
+					intDeck[j + 1] = intTemp;
+				}
+			}
+		}
+		// Reset used cards tracker
+		for (int i = 0; i < 52; i++) {
+			bolUsedCards[i] = false;
+		}
+	}
 }
